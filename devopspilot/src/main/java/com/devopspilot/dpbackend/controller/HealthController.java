0e9d0219c4102e31e65478edeca1341fb62d4eb6
neha shakari <nehashakarii@gmail.com>
2026-07-17 01:47:30 +0530

    Allow Vercel frontend CORS access

--- devopspilot/src/main/java/com/devopspilot/dpbackend/controller/HealthController.java
+++ devopspilot/src/main/java/com/devopspilot/dpbackend/controller/HealthController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://devops-pilot.vercel.app"
+})
 public class HealthController {
 
     @GetMapping("/api/health")
